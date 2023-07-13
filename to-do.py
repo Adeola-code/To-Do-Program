dcc21450f465c5a5d5e99288860bbee66cb8018c
Adeola-code <66573192+Adeola-code@users.noreply.github.com>
2023-07-13 13:29:12 +0100

    Add files via upload

---
+++ to-do.py
@@ -0,0 +1,78 @@
+from IPython.display import clear_output
+clear_output()
+
+
+task_list =[]
+def display_task(task_list):
+    print('Here are your current tasks')
+    print(task_list)
+# ADD TASKS
+def task_choice():
+    choice=input("What task would you like to add?: (Enter 'stop' to finish) ")
+    while choice.lower() != 'stop':
+        task_list.append(choice)
+        choice=input("What task would you like to add?: (Enter 'stop' to finish) ")
+        
+    clear_output()
+    
+#REMOVE TASKS
+def task_removal():
+    choice=input("What task would you like to remove?: (Enter 'skip' to leave list as it is) ")
+    while choice.lower() != 'skip':
+        try:
+            task_list.remove(choice)
+        except ValueError:
+            print('This task is not in the list. Please enter a valid task!')
+        choice=input("What task would you like to remove?: (Enter 'skip' to leave list as it is) ")
+    return(choice)
+    
+#CONTINUE OR STOP
+def taskon_choice():
+    choice = 'wrong'
+    while choice not in ['Y','N']:
+        choice = input("Would you like to add more tasks? Y or N ")
+
+        if choice not in ['Y', 'N']:
+            clear_output()
+
+            print("Oops! I didn't get that. Please make sure to choose Y or N")
+
+    if choice == "Y":
+        #Still wanna add more tasks
+        return True
+    else:
+        #No more tasks
+        return False
+
+    #OMO! TO SUM EVERYTHING AND KEEP IT RUNNING ALTOGETHER SHA
+taskon= True
+
+#First Task list
+
+
+
+while taskon:
+
+    #clear past output and show list
+    clear_output()
+    display_task(task_list)
+
+    #Choose Task to add
+    task_choice()
+
+
+    clear_output()
+    display_task(task_list)
+
+    #choose to remove task 
+    task_removal()
+
+    #clear screen and show new task list
+    clear_output()
+    display_task(task_list)
+
+    #To keep on adding
+    taskon= taskon_choice()
+
+
+
